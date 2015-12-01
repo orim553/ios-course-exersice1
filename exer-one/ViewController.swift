caa43410dbb98ff46a2d64889a174ed063057a69
ori mizrachi <orimizrachi@oris-MacBook-Air.local>
2015-12-01 17:16:17 +0200

    added files forthe first app

--- exer-one/ViewController.swift
+++ exer-one/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ivOrange: UIImageView!
+    @IBOutlet weak var ivApple: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBAction func btnHideOrange(sender: AnyObject) {
+        ivOrange.hidden = true
+        ivApple.hidden = false
+    }
 
+    @IBAction func btnHideApple(sender: AnyObject) {
+        ivOrange.hidden = false
+        ivApple.hidden = true
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
